f1ff4a24539f3972cc60e4a8d41f2f42c61aeb90
Naveen <sce22cs026@sairamtap.edu.in>
2023-12-03 16:15:02 +0530

    init

--- contents/main/branch1/j3.js
+++ contents/main/branch1/j3.js
@@ -1,14 +1,41 @@
-window.addEventListener('load', (event) => {
-    let Logout = document.getElementById("logout");
-    let isLogin = document.getElementById('isLogin');
+window.addEventListener("load", (event) => {
+  let Logout = document.getElementById("logout");
+  let isLogin = document.getElementById("isLogin");
 
+  Logout.addEventListener("click", () => {
+    localStorage.removeItem("isLogin");
+    localStorage.setItem("isLogin", JSON.stringify(false));
+
+    if (JSON.parse(localStorage.getItem("isLogin")) === false) {
+      window.location.href = "../index.html";
+    }
+  });
+
+  Post.addEventListener("click", () => {
+    let Url = document.getElementById("ImageUrl");
+    let Title = document.getElementById("description");
+    let Message = document.getElementById("message");
+    let Post = document.getElementById("Post");
+    let storage = [];
+    localStorage.setItem("storageList", JSON.stringify(storage));
     
-      Logout.addEventListener("click", () => {
-        localStorage.removeItem("isLogin");
-        localStorage.setItem("isLogin", JSON.stringify(false));
+    if (Url.value === "" || Title.value === "") {
+      alert('please fill url & title');
+    } else {
+      let msg = prompt("please enter password");
+      let memo = localStorage.getItem("password");
 
-        if (JSON.parse(localStorage.getItem("isLogin")) === false) {
-          window.location.href = "../index.html";
+      if (msg === memo) {
+        let existingList = localStorage.getItem("storageList");
+        if (existingList !== "") {
+          let store = { Imageurl: Url.value, title: Title.value };
+          storage.push(store);
+          localStorage.setItem("storageList", JSON.stringify(storage));
+          alert("message", "added", "succesfully ");
         }
-      });
+      } else {
+        alert('please prompt a correct password')
+      }
+    }
+  });
 });
